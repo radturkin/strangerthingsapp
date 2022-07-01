72bcb6fa14f6adf07dfec842c0d8850c780f8e43
Rad <bturkin@gmail.com>
2022-07-01 16:36:12 -0500

    first commits

---
+++ js/main.js
@@ -0,0 +1,16 @@
+let characters = ["Steve", "Nancy", "Robin", "Will", "Mike", "Joyce"];
+
+function mainCharacter(){
+    return characters[Math.floor(Math.random()*characters.length)];
+    
+}
+
+characterChosen = mainCharacter()
+console.log(characterChosen)
+
+srci="images/"+characterChosen+".png"
+console.log(srci)
+
+document.querySelector('img').src = srci
+
+document.querySelector('h1').innerText = `You are ${characterChosen}`

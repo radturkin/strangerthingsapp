6f139186be3195d673791f35cc502673c6f67f5f
Rad <bturkin@gmail.com>
2022-08-25 17:25:57 -0500

    cleanedup

--- js/main.js
+++ js/main.js
@@ -36,29 +36,54 @@ function chooser(){
     document.querySelector('button').addEventListener('click', mainCharacter)
 
 }
+function reset(){
+    botscore=0
+    paragraph=""
+    document.querySelector('p').innerText = ""
+    document.querySelector('#b2').innerText = ""
+    document.querySelector('#art2').classList.add("hidden")
+    document.querySelector('#b2').classList.add("hidden")
+    let el = document.getElementById('b1'),
+    elClone = el.cloneNode(true);
 
+    el.parentNode.replaceChild(elClone, el);
+    
+    document.querySelector('#char2').src = "blank.png"
+
+    char1=''
+    char2=''
+
+    document.getElementById("art2").style.display = 'none';
+    // hide the link
+    //btn.style.display = 'none';
+
+
+
+}
 // let storyArray = ["will be the main character this season. ", "will go missing and start the story. ",
 // "wants to investigate but ", "protests against this ", ", ", ", ", "and " ,"go off to search for them. ", 
 // "will find a clue while ", "solves it. Which leads them to believe that ","killed them. But they figure out that it was actually ", 
 // ". ", "and ","will sacrifice themselves to defeat the killer and ",  "will cry.But at the end we find out that ", 
 // "was responsible for these deaths and they get arrested. Ultimately ","was the most heroic character. "]
-
+let botscore=0
 function season5(){
+    reset()
+   
     let s5 = new Game(["will be the main character this season. ", "will go missing and start the story. ",
     "wants to investigate but ", "protests against this ", ", ", ", ", "and " ,"go off to search for them. ", 
     "will find a clue while ", "solves it. Which leads them to believe that ","killed them. But they figure out that it was actually ", 
     ". ", "and ","will sacrifice themselves to defeat the killer and ",  "will cry.But at the end we find out that ", 
     "was responsible for these deaths and they get arrested. Ultimately ","was the most heroic character. "], 
     "what happens season 5?", `Don't be such a ${charChosen}, select a new game.`)
-    let storyArray=s5.storyArray
-    localStorage.setItem("botscore",0)
-
+    storyArray=s5.storyArray
     document.querySelector('button').innerText = s5.buttonText
     document.querySelector('button').addEventListener('click', addLine)
 
 }
+localStorage.setItem("botscore",0)
 
 function mystery(){
+    reset()
     let mys = new Game(["dies.", "finds the body.", "is the most sad.", "is the lead detective.", 
     "is the first suspect.",
     "is their alibi.", ", ", ", and ", "are the final three suspects.", "is arrested.", 
@@ -66,14 +91,14 @@ function mystery(){
     "Solve the mystery", `Don't be such a ${charChosen}, select a new game.`)
     //let newArray = lineup.map(x=> `${mainCharacter()} ${x}`)
     //console.log(newArray)
-    let storyArray=mys.storyArray
-    localStorage.setItem("botscore",0)
+    storyArray=mys.storyArray
     document.querySelector('button').innerText = mys.buttonText
     document.querySelector('button').addEventListener('click', addLine)
 }
 
 function zombie(){
-    let zombieArray = ["will become a zombie but we don’t know why. " ,  
+    reset()
+    let zom = new Game(["will become a zombie but we don’t know why. " ,  
     "will discover the zombie and be so shocked that they call the police but it’s too late the zombie broke out and bit ",
     ", ", "and, ", ". The three of them become zombies and terrorize the town. ", ", ", ", ", ", ", ", ", "and ",
     " ,are the main group. And they are locked in an abandoned warehouse with ",", who became a zombie. ", ", kills the zombie. They find ",
@@ -82,15 +107,13 @@ function zombie(){
     "eventually finds the cure. But then we realize that it was ",
     "the doctor who caused the infection. And they are not happy the group found a cure so they kill ", "and ",
     ". But overall ",
-    "kills the corrupt doctor and everyone lives happy ever after(Or do they?)"]
-    let newzombie = zombieArray.map(x=> `${mainCharacter()} ${x}`)
-    console.log(newzombie)
+    "kills the corrupt doctor and everyone lives happy ever after(Or do they?)"], "Who has brains?", "too many zombies")
+    storyArray = zom.storyArray
+    document.querySelector('button').innerText = zom.buttonText
+    document.querySelector('button').addEventListener('click', addLine)
 
-    document.querySelector('p').innerText = newzombie.join(" ")
 
-    return newzombie
 }
-//addLine
 function life(){
 
 let lifelist=[`Your parents: ${mainCharacter()} , and ${mainCharacter()}`,
@@ -155,13 +178,15 @@ let dict = { "Steve":0, "Nancy":0, "Robin":0, "Will":0, "Mike":0, "Joyce":0, "Ba
 "Dr. Owens":0, "010":0, "008":0, 
 "Vickie":0, "Jason":0, "Fred":0, "Victor Creel":0, "Alexei":0, "Heather":0, "Jonathan":0, "Dr.Owens":0} 
 function thisthat(){
+    reset()
+    document.getElementById("art2").style.display = 'block';
+
     let char1=''
     let char2=''
-    let charsArray = characters
+    //let charsArray = characters
     char1 = mainCharacter()
     char2 = mainCharacter()
-    console.log(char1)
-    console.log(char2)
+    
     document.querySelector('button').innerText = "next this or that"
     document.querySelector('button').addEventListener('click', thisthat)
     document.querySelector('#b2').innerText = "tally favorite"
@@ -172,13 +197,10 @@ function thisthat(){
     let char2Image="images/"+char2+".png"
     document.querySelector('#mainImage').src = char1Image
     document.querySelector('#char2').src = char2Image
-    console.log(dict)
-    console.log(typeof char1)
+  
     let obj1 = document.getElementById("mainImage")
     obj1.onclick = function() {
-        console.log(dict[char1])
         dict[char1]+=1
-        console.log(dict)
         thisthat()
 
    
@@ -186,9 +208,7 @@ function thisthat(){
     let obj2 = document.getElementById("char2")
 
     obj2.onclick = function() {
-    console.log(dict[char2])
     dict[char2]+=1
-    console.log(dict)
     thisthat()
 
 }
@@ -238,7 +258,7 @@ if (!localStorage.getItem("botscore")){
 }
 
 function addLine(){
-    let botscore = Number(localStorage.getItem("botscore"))
+    botscore = Number(localStorage.getItem("botscore"))
 
     let charChosen= mainCharacter()
 

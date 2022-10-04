c789d88a8db25b6ebb0f071300452b927afc71fc
Rad <bturkin@gmail.com>
2022-10-04 18:59:50 -0500

    rpg

--- js/rpg.js
+++ js/rpg.js
@@ -1,14 +1,27 @@
-let obj={"Eleven":{
-    "color": "purple",
-    "type": "minivan",
-    "registration": new Date('2012-02-03'),
-    "capacity": 7
-  }}
+let charobj={"Eleven":{
+    "strength": 9,
+    "agility": 6,
+    "speed": 5,
+    "health": 125,
+    "attack":{"air bullying": 15, "roadkill":30},
+    "special": {"nina project": 100}
+  }
+}
+
+let villainobj = {"001":{
+  "strength": 10,
+  "agility": 4,
+  "speed": 5,
+  "health": 200,
+  "attack":{a:30,b:50},
+  "special":{"freeze":100}
+  }
+}
 var x
 function changeResult() {
     x = document.getElementById("char-select").value;
     document.getElementById("result").innerHTML = "You selected: " + x;
-    console.log(obj["Eleven"])
+    console.log(charobj["Eleven"])
     if (x==="Eleven"){
         console.log("Its going to be okay")
     }
@@ -31,3 +44,15 @@ function enterGame(x){
 //adjust object values
 //return object
 //other functions 
+
+console.log(charobj)
+console.log(villainobj)
+
+//adjust to be total strength as variable for attack times handicap
+function d20(){
+  roll=Math.ceil(Math.random()*20)
+  return roll
+}
+let attack=d20()
+
+console.log(attack)
